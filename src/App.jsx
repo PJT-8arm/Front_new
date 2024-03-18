257d0b99ea2fa880649e50a70e626127380968ba
geniushee <h1116jeon@gmail.com>
2024-03-18 18:34:07 +0900

    feat : body 크기 설정 - 데이지ui 테마 적용시 색상 분리 해결

--- src/App.jsx
+++ src/App.jsx
@@ -18,34 +18,36 @@ function App() {
   return (
     <>
       <Router>
-        <button className="btn btn-secondary" onClick={() => {
-          setlogin(isLogined ? false : true);
-          console.log("isLogined:", isLogined);
-        }}>{isLogined ? '로그아웃' : '로그인'}</button>
-        <main>
-        <Routes>
-          {/* 약속 화면으로 접근했을 때 보여줄 컴포넌트 */}
-          <Route path="/appointment" element={<Appointment />}></Route>
-          {/* 랭킹 화면으로 접근했을 때 보여줄 컴포넌트 */}
-          <Route path="/ranking" element={<Ranking />}></Route>
-          {/* 채팅 화면으로 접근했을 때 보여줄 컴포넌트 */}
-          <Route path="/chat" element={<Chat />}></Route>
-          {/* 마이페이지 화면으로 접근했을 때 보여줄 컴포넌트 */}
-          <Route path="/mypage" element={<Mypage />}></Route>
-          {/* 로그인 화면으로 접근했을 때 보여줄 컴포넌트 */}
-          <Route path="/login" element={<LoginPage />} />
-          {/* 회원가입 페이지 */}
-          <Route path="/register" element={<RegisterPage />} />
-          {/* 메인 페이지로 접근했을 때 보여줄 컴포넌트 */}
-          <Route path="/" element={<Home />} />
-          {/* 상세 페이지의 경로와 매칭될 컴포넌트 */}
-          <Route path="/recruitments/detail/:id" element={<RecruitmentDetail />} />
-          {/* 다른 모든 경로에 대한 처리. 404 페이지나 메인 페이지 리다이렉트 등의 로직을 추가할 수 있습니다. */}
-        </Routes>
-        </main>
-        <footer>
-          <Navbar isLogined={isLogined} />
-        </footer>
+        <div className='flex flex-col items-center'>
+          <main className='min-h-full'>
+            <button className="btn btn-primary" onClick={() => {
+              setlogin(isLogined ? false : true);
+              console.log("isLogined:", isLogined);
+            }}>{isLogined ? '로그아웃' : '로그인'}</button>
+            <Routes>
+              {/* 약속 화면으로 접근했을 때 보여줄 컴포넌트 */}
+              <Route path="/appointment" element={<Appointment />}></Route>
+              {/* 랭킹 화면으로 접근했을 때 보여줄 컴포넌트 */}
+              <Route path="/ranking" element={<Ranking />}></Route>
+              {/* 채팅 화면으로 접근했을 때 보여줄 컴포넌트 */}
+              <Route path="/chat" element={<Chat />}></Route>
+              {/* 마이페이지 화면으로 접근했을 때 보여줄 컴포넌트 */}
+              <Route path="/mypage" element={<Mypage />}></Route>
+              {/* 로그인 화면으로 접근했을 때 보여줄 컴포넌트 */}
+              <Route path="/login" element={<LoginPage />} />
+              {/* 회원가입 페이지 */}
+              <Route path="/register" element={<RegisterPage />} />
+              {/* 메인 페이지로 접근했을 때 보여줄 컴포넌트 */}
+              <Route path="/" element={<Home />} />
+              {/* 상세 페이지의 경로와 매칭될 컴포넌트 */}
+              <Route path="/recruitments/detail/:id" element={<RecruitmentDetail />} />
+              {/* 다른 모든 경로에 대한 처리. 404 페이지나 메인 페이지 리다이렉트 등의 로직을 추가할 수 있습니다. */}
+            </Routes>
+          </main>
+          <footer className='h-16 w-full'>
+            <Navbar isLogined={isLogined} />
+          </footer>
+        </div>
       </Router>
 
     </>
