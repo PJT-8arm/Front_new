f3610fa83e1cd4e1e58d54a35cb5f907fd94bc00
taehoon <xogns2134@gmail.com>
2024-03-18 15:01:13 +0900

    fix: merge

--- src/components/signUp/RegisterPage.jsx
+++ src/components/signUp/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './components/signUp/signup.css';
+import './signup.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
