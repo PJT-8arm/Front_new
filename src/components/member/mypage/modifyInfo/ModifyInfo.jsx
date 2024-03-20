05cd5428abdd20eab02b7b54b484e969ec6ee31f
geniushee <h1116jeon@gmail.com>
2024-03-20 20:33:28 +0900

    feat : 회원정보 수정 마무리 및 프로파일.jsx 생성

--- src/components/member/mypage/modifyInfo/ModifyInfo.jsx
+++ src/components/member/mypage/modifyInfo/ModifyInfo.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useMypageDetails, useMypageModifyDetails } from '../../../../openapi/orval_query/api/mypage-controller/mypage-controller';
-import { Form } from 'react-router-dom';
+import { Form, Link } from 'react-router-dom';
 
 function ModifyInfo(props) {
-    const { data: memberDto, isLoading : loadingGet, isError: ErrorGet } = useMypageDetails();
-    const {mutate: postData, isLoading : loadingPost, isError : ErrorPost } = useMypageModifyDetails()
+    const { data: memberDto, isLoading: loadingGet, isError: ErrorGet } = useMypageDetails();
+    const { mutate: postData, isLoading: loadingPost, isError: ErrorPost } = useMypageModifyDetails()
     // console.log("memberDto : ", memberDto);
     // console.log("memberDto.name : ", memberDto.name);
     // console.log("refname:", refName.current.value);
@@ -32,10 +32,10 @@ function ModifyInfo(props) {
     }, [memberDto])
 
     const handleChange = (e) => {
-        const {id, value} = e.target;
+        const { id, value } = e.target;
         setFormdata(preFormdata => ({
             ...preFormdata,
-            [id] : value,
+            [id]: value,
         }));
     }
 
@@ -50,8 +50,8 @@ function ModifyInfo(props) {
             prePassword: formdata.prePassword,
             username: formdata.username,
         }
-        
-        await postData({data : memberModifyDto});
+
+        await postData({ data: memberModifyDto });
     }
 
     if (loadingGet || loadingPost) {
@@ -68,28 +68,32 @@ function ModifyInfo(props) {
 
     return (
         <>
-            <form action="POST" onSubmit={onSubmitHandler}>
-                <label className="input input-bordered flex items-center gap-2">
+        <h1 className='font-bold text-lg m-2'>회원정보 수정</h1>
+            <form className='m-2' action="POST" onSubmit={onSubmitHandler}>
+                <label className="input input-bordered flex items-center gap-2 mt-2">
                     아이디
                     <div className="grow">{formdata.username}</div>
                 </label>
-                <label className="input input-bordered flex items-center gap-2">
+                <label className="input input-bordered flex items-center gap-2 mt-2">
                     변경전 비밀번호
                     <input type="password" className="grow" placeholder="******" id='prePassword' value={formdata.prePassword} onChange={handleChange} />
                 </label>
-                <label className="input input-bordered flex items-center gap-2">
+                <label className="input input-bordered flex items-center gap-2 mt-2">
                     변경후 비밀번호
-                    <input type="password" className="grow" placeholder="******" id='postPassword' value={formdata.postPassword} onChange={handleChange}/>
+                    <input type="password" className="grow" placeholder="******" id='postPassword' value={formdata.postPassword} onChange={handleChange} />
                 </label>
-                <label className="input input-bordered flex items-center gap-2">
+                <label className="input input-bordered flex items-center gap-2 mt-2">
                     이름
-                    <input type="text" className="grow" placeholder="이름을 적어주세요." id='name' value={formdata.name} onChange={handleChange}/>
+                    <input type="text" className="grow" placeholder="이름을 적어주세요." id='name' value={formdata.name} onChange={handleChange} />
                 </label>
-                <label className="input input-bordered flex items-center gap-2">
+                <label className="input input-bordered flex items-center gap-2 mt-2">
                     닉네임
-                    <input type="text" className="grow" placeholder="닉네임을 적어주세요" id='nickname' value={formdata.nickname} onChange={handleChange}/>
+                    <input type="text" className="grow" placeholder="닉네임을 적어주세요" id='nickname' value={formdata.nickname} onChange={handleChange} />
                 </label>
-                <button type='submit'>수정</button>
+                <div className='flex justify-center items-center'>
+                    <button className='btn btn-primary m-4' type='submit'>수정</button>
+                    <button className='btn btn-primary m-4'><Link to="/mypage/list">이전</Link></button>
+                </div>
             </form>
         </>
     );
