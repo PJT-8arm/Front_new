d03007c2d9aa17d54ac78809fa2c05d5d3d0608a
geniushee <h1116jeon@gmail.com>
2024-03-21 16:10:04 +0900

    feat : 나의 모집글 페이지 구현

--- src/components/member/mypage/myrecruitment/Myrecruitment.jsx
+++ src/components/member/mypage/myrecruitment/Myrecruitment.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMypageMyRecruitment } from '../../../../openapi/orval_query/api/mypage-controller/mypage-controller';
+import { Link } from 'react-router-dom';
+import RecruitmentDetail from '../../../recuitment/RecruitmentDetail';
 
-function RecruitmentCard({item, index}) {
+function RecruitmentCard({ item, index }) {
 
-    console.log(item, index)
-    console.log(item['recruitmentDto'], index)
+    const [card, setCard] = useState({
+        id: "",
+        title: "",
+        content: "",
+        recruit_date: "",
+        duration: "",
+        routine: "",
+    })
+
+    useEffect(() => {
+        const recruite_date = item['recruitmentDto']['recruit_date']
+        const date = recruite_date.split("T")[0].split("-");
+        const time = recruite_date.split("T")[1].split(".")[0].split(":");
+        const duration = item['recruitmentDto']['duration'].split(":")
+
+        setCard({
+            ...item['recruitmentDto'],
+            recruit_date: date[0].slice(-2) + "년 " + date[1] + "월 " + date[2] + "일 " + time[0] + ":" + time[1],
+            duration : duration[0] + "시간 " + duration[1] + "분",
+        })
+    }, [item])
+
     return (
-        <article className='border' key={index}>
-            <div>
-                제목 : {item['recruitmentDto'].title}
-            </div>
-            <div>
-                내용 : {item['recruitmentDto'].content}
-            </div>
-            <div>
-                날짜 : {item['recruitmentDto'].recruit_date}
-            </div>
-            <div>
-                시간 : {item['recruitmentDto'].recruit_time}
-            </div>
-            <div>
-                운동루틴 : {item['recruitmentDto'].routine}
-            </div>
-        </article>
+        <>
+            <Link className="card w-3/4 bg-base-100 shadow-sm hover:shadow-2xl m-2"
+             to='/recruitments/detail/:id' element={<RecruitmentDetail />}>
+                    <div className="card-body">
+                        <h2 className="card-title">{card.title}</h2>
+                        <div className='text-sm'>
+                        <p>- 운동일자 : {card.recruit_date}</p>
+                        <p>- 운동시간 : {card.duration}</p>
+                        <p>- 운동루틴 : {card.routine}</p>
+                        </div>
+                    </div>
+            </Link>
+        </>
     )
 }
 
@@ -46,9 +64,15 @@ function Myrecruitment(props) {
 
     return (
         <>
-            {myrecruitmentList.map((item, index) => (
-                <RecruitmentCard item={item} index={index} />
-            ))}
+            <h2 className='mx-2 font-bold text-lg'>나의 모집글</h2>
+            <section className='flex flex-col w-full items-center'>
+                {myrecruitmentList.map((item, index) => (
+                    <RecruitmentCard item={item} index={index} />
+                ))}
+            </section>
+            <div className='flex justify-center items-center'>
+                <button className='btn btn-primary m-4'><Link to="/mypage/list">이전</Link></button>
+            </div>
         </>
     );
 }
