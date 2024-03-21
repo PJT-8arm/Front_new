8e7ad830bacee90bb798c6af03bdbb6fa1d548a1
geniushee <h1116jeon@gmail.com>
2024-03-21 17:39:21 +0900

    refactor : 마이페이지에서 불필요한 부분 주석 처리

--- src/components/member/mypage/MypageList.jsx
+++ src/components/member/mypage/MypageList.jsx
@@ -37,14 +37,15 @@ function MypageList(props) {
                     name={<span className='mx-2'>회원 정보 수정</span>}
                     />
                 </article>
-                <article className='mt-2'>
+                {/* 불필요한 부분 주석 처리
+                 <article className='mt-2'>
                     <h2 className='mt-4 font-bold text-lg'>기타</h2>
                     <LinkSetter goto={'/mypage/qna'}
                     addedClassName={'flex items-center mx-4 mt-2'}
                     icon={<svg className='w-8 h-8' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>}
                     name={<span className='mx-2'>문의하기</span>}
                     />
-                </article>
+                </article> */}
             </section>
         </>
     );
