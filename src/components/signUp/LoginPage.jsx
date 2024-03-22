56fde549aa44cdd34d1b31990e41553c2c3e8e8e
YUBEENLIM <bgs0782@gmail.com>
2024-03-22 15:56:05 +0900

    refactor : 회원가입, 로그인 일부 코드 수정 및 css 추가

--- src/components/signUp/LoginPage.jsx
+++ src/components/signUp/LoginPage.jsx
@@ -26,20 +26,37 @@ function LoginPage() {
             setErrorMessage("로그인에 실패했습니다. 다시 시도해주세요.");
         }
     };
+
+    // 회원가입 페이지로 이동하는 함수
+    const goToRegisterPage = () => {
+        navigate("/register");
+    };
     
 
     return (
         <div className="container">
             <form className="login-form" onSubmit={onSubmitHandler}>
-                <label>아이디:</label>
+                <div className='title'>
+                    <p>8Arm💪</p>
+                </div>
+                <div className="label-container">
+                    <div className='content'>
+                        <label>아이디</label>
+                    </div>
                 <input type="text" ref={usernameRef} />
-                <label>비밀번호:</label>
+                </div>
+                <div className="label-container">
+                    <div className='content'>
+                        <label>비밀번호</label>
+                    </div>
                 <input type="password" ref={passwordRef} />
                 {isError && <p style={{ color: 'red' }}>{errorMessage || error}</p>}
-                <button type="submit" className="login-button">Login</button> {/* 로그인 버튼에 클래스 추가 */}
-
+                </div>
+                <div className='button-container'>
+                    <button type="submit" className="login-button">로그인</button> {/* 로그인 버튼에 클래스 추가 */}
+                </div>
                 {/* 버튼으로 회원가입 페이지로 이동 */}
-                <Link to="/register" className="signup-link">회원가입</Link> {/* 회원가입 링크에 클래스 추가 */}
+                <button type="button" className="signup-link" onClick={goToRegisterPage}>📝회원가입</button> {/* 회원가입 버튼 추가 */}
             </form>
             <div style={{ minHeight: '50px' }}></div> {/* 로그인 버튼이 항상 표시되도록 빈 div 추가 */}
         </div>
