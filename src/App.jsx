b339b9945603d5bb4f847e2a897d250bfdf2667a
jinyoung121636 <damin0518@hanmail.net>
2024-03-22 13:46:47 +0900

    refactor : clone 후home.jsx 추가

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import ModifyInfo from './components/member/mypage/modifyInfo/ModifyInfo';
 import MypageList from './components/member/mypage/MypageList';
 import Profile from './components/member/mypage/profile/Profile';
 
+
 function App() {
   const [isLogined, setlogin] = useState(false);
 
@@ -23,6 +24,18 @@ function App() {
       <Router>
         <div className='flex flex-col items-center'>
           <main className='min-h-full min-w-full'>
+          <div className='Topbar' style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                <div className='logo' style={{margin:'0.5rem 1rem 0rem 1rem'}}>
+                    <img src='/src/logo.png' alt='로고 이미지'/>
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin:'0.5rem 1rem 0rem 1rem'}}>
+                  <label style={{ height: 40, width: 180 }} className="input input-bordered flex items-center">
+                      <input style={{ paddingRight: '2rem' }} type="text" className="grow" placeholder="Search" />
+                      <svg style={{ position: 'absolute', right: 0, marginRight: '1.5rem' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                  </label>
+                </div>
+            </div>
+
             <Routes>
               {/* 약속 화면으로 접근했을 때 보여줄 컴포넌트 */}
               {/* <Route path="/appointment" element={<Appointment />}></Route> */}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './home.css'
 
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    );
+
+  const [recruitmentData, setRecruitmentData] = useState([]);
+
+  useEffect(() => {
+    // recruitmentList API 호출
+    const fetchRecruitmentData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/recruitments/list');
+        setRecruitmentData(response.data);
+
+      } catch (error) {
+        console.error('Error fetching recruitment data:', error);
+      }
+    };
+
+    fetchRecruitmentData(); // 컴포넌트가 마운트될 때 API 호출
+  }, []); // useEffect가 한 번만 실행되도록 빈 배열을 두 번째 인수로 전달
+
+  if (recruitmentData.length === 0) return <div>로딩중...</div>;
+  
+ // 날짜 형식 변환 함수
+ const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hour = ('0' + date.getHours()).slice(-2);
+    const minute = ('0' + date.getMinutes()).slice(-2);
+    return `${year}-${month}-${day} ${hour}:${minute}`;
+  };
+
+  return (
+    <div>
+      <ul>
+        {/* recruitmentData를 반복하여 각 아이템을 표시합니다. */}
+        {recruitmentData.map((item, index) => (
+          <li key={index}>
+            <div>
+                <div style={{ display: 'flex' , justifyContent: 'center' }}>
+                    <div className="card w-90 bg-base-100 shadow-xl">
+                            <div className="cardbody">
+                                <div className="avatar">
+                                    <img src={item.memberInfoDto.imgUrl} className="rounded-full" alt="avatar" />
+                                </div>
+                                <div className='content'>
+                                    <div className="badge badge-primary">
+                                        <p className='nickname'>{item.memberInfoDto.nickname}</p>
+                                    </div>                                   
+                                    <h2 className="title">{item.recruitmentDto.title}</h2>
+                                    <div className='tag'>
+                                        <p className='tag-list'>{'#'+ item.recruitmentDto.partnerAge}</p>
+                                        <p className='tag-list'>{'#'+ item.recruitmentDto.partnerGender}</p>
+                                        <p className='tag-list'>{'#'+ item.recruitmentDto.place}</p>
+
+                                    </div>
+                                    <p className='date'>{formatDate(item.recruitmentDto.recruit_date)}</p>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
