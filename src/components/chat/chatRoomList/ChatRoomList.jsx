5ab9299c8a1697da8c45f4088edc3e7f0b114703
taehoon <xogns2134@gmail.com>
2024-03-24 21:15:47 +0900

    design: 채팅방 목록 ui 개선, flex가 안되는 문제

--- src/components/chat/chatRoomList/ChatRoomList.jsx
+++ src/components/chat/chatRoomList/ChatRoomList.jsx
@@ -10,15 +10,16 @@ const ChatRoomList = () => {
     // 날짜를 MM/DD 형식으로 포맷하는 함수
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        return date.toLocaleDateString('en-US',
-            {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: false
-            });
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1 처리
+        const day = String(date.getDate()).padStart(2, '0');
+        let hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+
+        // 24시간 형식으로 시간을 표시하고, 한 자릿수 시간에는 0을 붙이지 않음
+        const formattedTime = `${year}-${month}-${day} ${hours}:${minutes}`;
+
+        return formattedTime;
     };
 
     // 채팅방을 클릭했을 때 호출되는 함수
@@ -27,31 +28,26 @@ const ChatRoomList = () => {
     };
 
     return (
-        <div>
+        <>
             {data && data.map((chatRoom) => (
-                <div key={chatRoom.chatRoomId} className="hover" onClick={() => handleRoomClick(chatRoom.chatRoomId)}>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
-                                <img src={chatRoom.imgUrl} alt="Avatar" />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="font-bold">{chatRoom.chatRoomName}</div>
-                        </div>
+                <div className='card bg-base-100 shadow-xl chat-list-container' key={chatRoom.chatRoomId} onClick={() => handleRoomClick(chatRoom.chatRoomId)}>
+                    <div className="chat-list-img">
+                        <img src={chatRoom.imgUrl} alt="채팅방 이미지" />
                     </div>
-
                     <div>
-                        <div className="date-size text-sm opacity-50">
-                            {formatDate(chatRoom.lastMessageDate)}
+                        <div className='chat-list-name-date'>
+                            <div className="font-bold">{chatRoom.chatRoomName}</div>
+                            <strong className="chat-room-message-date">
+                                {formatDate(chatRoom.lastMessageDate)}
+                            </strong>
                         </div>
-                        <div className="text-ellipsis overflow-hidden max-w-xs whitespace-nowrap">
-                        {chatRoom.lastMessageContent}
+                        <div className="chat-room-message-content">
+                            {chatRoom.lastMessageContent}
                         </div>
                     </div>
                 </div>
             ))}
-        </div>
+        </>
     );
 };
 
