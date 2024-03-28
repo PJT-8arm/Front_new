a7ed3760fb5a97442eabc0aa9c2ededa55f1c962
taehoon <xogns2134@gmail.com>
2024-03-28 17:08:20 +0900

    대화 신청 버튼 추가 (#46)
    * feat: 브라우저 캐시 설정 & 채팅기능 deprecated 수정
    
    * feat: 회원가입에 프로필 사진 업로드 기능 추가
    
    * refactor: 이미지 위치 /public으로 변경
    
    * refactor: 이미지 위치 /public/images로 이동
    
    * feat: 모집상세에 대화신청 버튼 추가
    
    * fix: axios함수 사용법 수정
    
    * fix: merge

---
+++ src/components/recuitment/RecruitmentDetail.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useRecruitmentDetails } from '../../openapi/orval_query/api/recruitments/recruitments';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { FaUserAlt, FaBirthdayCake, FaVenusMars, FaWeightHanging } from 'react-icons/fa';
+import { MdPlace, MdSchedule } from 'react-icons/md';
+import './RecruitmentDetail.css';
+import { axiosInstance } from '../../utils/axiosInstance';
+import CreateAppointmentButton from '../appointment/CreateAppointmentButton';
+
+const RecruitmentDetail = () => {
+  const { id } = useParams();
+  const recruitmentId = parseInt(id, 10);
+  const navigate = useNavigate();
+
+  const { data: detail, isLoading, error } = useRecruitmentDetails(recruitmentId);
+
+
+  const handleMakeChatRoom = async (name) => {
+    const roomId = await axiosInstance({
+      url: `api/chat/room/make/${name}`, // 실제 theirName 값으로 URL을 동적으로 구성
+      method: 'get', // 요청 메서드 지정
+    });
+    console.log(roomId);
+    navigate(`/chat/room/${roomId}`); // 해당 roomId를 가진 URL로 이동
+  }
+  // 약속 생성을 위한 함수
+  const handleCreateAppointment = async () => {
+    if (!detail) return; // 데이터가 없을 때 함수 종료
+    // 약속 생성 로직 추가
+    console.log('약속을 생성합니다.');
+  };
+
+  if (isLoading) return <div className="flex justify-center items-center p-4">로딩중...</div>;
+  if (error) return <div className="alert alert-error shadow-lg text-center p-4"><div>{error.message}</div></div>;
+
+  return (
+    <div className="max-w-lg mx-auto p-4 bg-base-100 shadow-2xl rounded-lg">
+      <div className="mb-6">
+        <h2 className="text-2xl font-bold mb-4 border-b pb-2">{detail?.recruitmentDto?.title}</h2>
+        <p className="mb-4">{detail?.recruitmentDto?.content}</p>
+        <div className="text-sm mb-4 space-y-2">
+          <p className="flex items-center"><MdSchedule className="mr-2" />모집 날짜: <span className="font-semibold ml-1">{detail?.recruitmentDto?.recruit_date}</span></p>
+          <p className="flex items-center"><MdPlace className="mr-2" />장소: <span className="font-semibold ml-1">{detail?.recruitmentDto?.place}</span></p>
+          <p className="flex items-center"><FaVenusMars className="mr-2" />파트너 성별: <span className="font-semibold ml-1">{detail?.recruitmentDto?.partnerGender}</span></p>
+          <p className="flex items-center"><FaBirthdayCake className="mr-2" />파트너 나이: <span className="font-semibold ml-1">{detail?.recruitmentDto?.partnerAge}</span></p>
+          <p className="flex items-center"><FaUserAlt className="mr-2" />루틴: <span className="font-semibold ml-1">{detail?.recruitmentDto?.routine}</span></p>
+          <p className="flex items-center"><FaWeightHanging className="mr-2" />지속 시간: <span className="font-semibold ml-1">{detail?.recruitmentDto?.duration}</span></p>
+        </div>
+      </div>
+
+      <div className="mb-6">
+        <h3 className="text-xl font-bold mb-2">회원 정보</h3>
+        <div className="flex items-center">
+          {detail?.memberInfoDto?.imgUrl && (
+            <img src={detail.memberInfoDto.imgUrl} alt="Profile" className="w-20 h-20 rounded-full mr-4 shadow-lg" />
+          )}
+          <div className='flex-grow-div'>
+            <div className='contain-button'>
+              <p className="flex items-center"><FaUserAlt className="mr-2" />이름: <span className="font-semibold ml-1">{detail?.memberInfoDto?.name}</span></p>
+              <button className='btn' onClick={() => handleMakeChatRoom(detail.memberInfoDto.name)}> 대화신청 </button>
+            </div>
+            <p className="flex items-center"><FaUserAlt className="mr-2" />장소: <span className="font-semibold ml-1">{detail?.memberInfoDto?.address}</span></p>
+          </div>
+        </div>
+      </div>
+
+
+      <div>
+        <h3 className="text-xl font-bold mb-2">프로필 정보</h3>
+        <p className="flex items-center"><FaBirthdayCake className="mr-2" />나이: <span className="font-semibold ml-1">{detail?.profileDto?.age}</span></p>
+        <p className="flex items-center"><FaVenusMars className="mr-2" />성별: <span className="font-semibold ml-1">{detail?.profileDto?.gender}</span></p>
+        <p className="flex items-center"><FaWeightHanging className="mr-2" />벤치프레스: <span className="font-semibold ml-1">{detail?.profileDto?.benchPress}</span></p>
+        <p className="flex items-center"><FaWeightHanging className="mr-2" />데드리프트: <span className="font-semibold ml-1">{detail?.profileDto?.deadLift}</span></p>
+        <p className="flex items-center"><FaWeightHanging className="mr-2" />스쿼트: <span className="font-semibold ml-1">{detail?.profileDto?.squat}</span></p>
+      </div>
+      {/* 약속 생성 버튼 */}
+      <CreateAppointmentButton handleCreateAppointment={handleCreateAppointment} />
+    </div>
+  );
+};
+
+export default RecruitmentDetail;
